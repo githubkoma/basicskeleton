62c126432080ac5975d5c864b59211edc402489e
githubkoma <iggy@ubuntu-22-04.fedora>
2023-11-19 09:23:40 +0100

    removed comment

--- templates/index.php
+++ templates/index.php
@@ -3,8 +3,6 @@ script('basicskeleton', 'script');
 style('basicskeleton', 'style');
 ?>
 
-<!--https://vueflow.dev/examples/empty.html-->
-
 <div id="content" style="margin-top: -1px; margin-left: -1px;">
 	<div id="app-navigation">
 		<?php print_unescaped($this->inc('navigation/index')); ?>
@@ -15,4 +13,3 @@ style('basicskeleton', 'style');
 		<?php print_unescaped($this->inc('content/index')); ?>
 	</div>
 </div>
-
